958f2397260536b5a41f55735b400fe9aeb52f99
Jose <jozylopez27@gmail.com>
2020-06-22 15:42:07 -0500

    checkerboard rendering

--- components/Checkerboard.js
+++ components/Checkerboard.js
@@ -2,15 +2,48 @@ import React, { Component } from 'react'
 
 
 export default class Checkerboard extends Component {
-    state = { value: null }
+    state = { value: 8 }
+
+    createBoard = () => {
+        let arr = []
+
+        for(let i = 0; i < this.state.value; i++) {
+            if(i % 2 === 1) {
+                arr.push('_')
+            } else if(i % 2 === 0) {
+                arr.push('#')
+            }
+        }
+
+        return arr.map(el => el === '#' ? 
+        <div className='hash'>#</div>: <div className='space'>_</div>)
+    }
+
+
     render() {
         return (
             <>
-            <div>hello world</div>
+            <div className='boardContainer'>{this.createBoard()}</div>
             <form>
                 <label htmlFor='checkerVal'>input checkerboard val</label>
                 <input id='checkerVal'></input>
             </form>
+            <style jsx>{`
+        .boardContainer {
+            margin: 50px auto 20px auto;
+            border: 1px solid black;
+            display: flex;
+        }
+
+        .space {
+            background-color: black;
+            border: 1px solid black;
+        }
+        .hash {
+            background-color: white;
+            border: 1px solid black;
+        }
+      `}</style>
             </>
         )
     }
